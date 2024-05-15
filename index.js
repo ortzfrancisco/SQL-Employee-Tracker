0c1a40c74c3a273f4257160df5e8f424cfd2740f
francisco ortiz <franciscoortz89@gmail.com>
2024-05-14 22:47:04 -0400

    updated db files

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // Variable Definitions & Dependencies
 const inquirer = require('inquirer');
-const db = require('./db/connection');
-
+const mysql = require('mysql');
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'Elpueblito1',
+        database: 'employee'
+    },
+    console.log(`Connected to the courses_db database.`)
+);
 // Start server after DB connection
 db.connect(err => {
     if (err) throw err;
@@ -247,7 +258,7 @@ var employee_tracker = function () {
                         }
                     }
 
-                    db.query(`UPDATE employee SET ? WHERE ?`, [{role_id: role}, {last_name: name}], (err, result) => {
+                    db.query(`UPDATE employee SET ? WHERE ?`, [{ role_id: role }, { last_name: name }], (err, result) => {
                         if (err) throw err;
                         console.log(`Updated ${answers.employee} role to the database.`)
                         employee_tracker();
